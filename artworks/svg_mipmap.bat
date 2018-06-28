6d8c3956d640225010a11942a74f95b35e649f09
Seb <sebastien@yoctopuce.com>
2018-06-28 16:53:10 +0200

    add artworks

---
+++ artworks/svg_mipmap.bat
@@ -0,0 +1,5 @@
+"c:\Program Files\Inkscape\inkscape.exe" %1.svg -e ..\app\src\main\res\mipmap-mdpi\%1.png -C -d 96
+"c:\Program Files\Inkscape\inkscape.exe" %1.svg -e ..\app\src\main\res\mipmap-hdpi\%1.png -C -d 144
+"c:\Program Files\Inkscape\inkscape.exe" %1.svg -e ..\app\src\main\res\mipmap-xhdpi\%1.png -C -d 192
+"c:\Program Files\Inkscape\inkscape.exe" %1.svg -e ..\app\src\main\res\mipmap-xxhdpi\%1.png -C -d 288
+"c:\Program Files\Inkscape\inkscape.exe" %1.svg -e ..\app\src\main\res\mipmap-xxxhdpi\%1.png -C -d 384
