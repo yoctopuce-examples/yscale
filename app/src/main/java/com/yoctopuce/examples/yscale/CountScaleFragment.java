e2088da3b89ee044ad0318fd3591a9c6ee28546e
Sebastien Rinsoz <sebastien@yoctopuce.com>
2019-10-14 17:23:49 +0200

    Update Android SDK

--- app/src/main/java/com/yoctopuce/examples/yscale/CountScaleFragment.java
+++ app/src/main/java/com/yoctopuce/examples/yscale/CountScaleFragment.java
@@ -1,7 +1,7 @@
 package com.yoctopuce.examples.yscale;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
